f3ce4efc806f8964bcc9b118e67b59dc936449cc
renanhomiak <renanhomiakg@gmail.com>
2026-03-24 15:19:32 -0300

    primeiro commit

---
+++ src/main/java/com/homiak/ex06/Ex06Application.java
@@ -0,0 +1,13 @@
+package com.homiak.ex06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex06Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/homiak/ex06/models/ClienteModel.java
@@ -0,0 +1,30 @@
+package com.homiak.ex06.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cliente")
+public class ClienteModel {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_cliente")
+    private String nome;
+
+    @Column(name = "email_cliente")
+    private String email;
+
+    private String telefone;
+
+    public Long getId() { return id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+}
